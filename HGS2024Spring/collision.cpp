bc239c063a54cdc6774ab80459871dee506ecf5a
RitsukiObara <221134@st.yoshida-g.ac.jp>
2024-03-25 00:47:01 +0900

    ソース挿入

---
+++ HGS2024Spring/collision.cpp
@@ -0,0 +1,299 @@
+//===========================================
+//
+// 当たり判定のメイン処理[collision.cpp]
+// Author 小原立暉
+//
+//===========================================
+//*******************************************
+// インクルードファイル
+//*******************************************
+#include "game.h"
+#include "collision.h"
+#include "shadowCircle.h"
+#include "objectElevation.h"
+#include "useful.h"
+
+//===============================
+// マクロ定義
+//===============================
+namespace
+{
+	const float COLLISION_ADD_DIFF_LENGTH = 0.01f;	// 僅かな誤差を埋めるためのマクロ定義(突っかかり防止)
+}
+
+//===============================
+// 起伏地面の範囲外の当たり判定
+//===============================
+bool collision::ElevOutRangeCollision(D3DXVECTOR3* pPos, const D3DXVECTOR3& posOld, const float fWidth)
+{
+	// ローカル変数宣言
+	bool bCollision = false;									// 当たり判定状況
+	CListManager<CElevation*> list = CElevation::GetList();
+	CElevation* pElev = nullptr;			// 先頭の小判
+	CElevation* pElevEnd = nullptr;		// 末尾の値
+	int nIdx = 0;
+
+	if (list.IsEmpty() == false)
+	{ // 空白じゃない場合
+
+		// 先頭の値を取得する
+		pElev = list.GetTop();
+
+		// 末尾の値を取得する
+		pElevEnd = list.GetEnd();
+
+		while (true)
+		{ // 無限ループ
+
+			if (pElev->GetPos().z + (pElev->GetSize().z * 0.5f) >= pPos->z &&
+				pElev->GetPos().z - (pElev->GetSize().z * 0.5f) <= pPos->z &&
+				pElev->GetPos().y > pPos->y)
+			{ // 起伏地面より下にいる場合
+
+				if (posOld.x + fWidth <= pElev->GetPos().x - (pElev->GetSize().x * 0.5f) &&
+					pPos->x + fWidth >= pElev->GetPos().x - (pElev->GetSize().x * 0.5f))
+				{ // 左の当たり判定
+
+					// 位置を設定する
+					pPos->x = pElev->GetPos().x - (pElev->GetSize().x * 0.5f) - fWidth;
+
+					// 当たり判定状況を true にする
+					bCollision = true;
+				}
+				else if (posOld.x - fWidth >= pElev->GetPos().x + (pElev->GetSize().x * 0.5f) &&
+					pPos->x - fWidth <= pElev->GetPos().x + (pElev->GetSize().x * 0.5f))
+				{ // 右の当たり判定
+
+					// 位置を設定する
+					pPos->x = pElev->GetPos().x + (pElev->GetSize().x * 0.5f) + fWidth;
+
+					// 当たり判定状況を true にする
+					bCollision = true;
+				}
+			}
+
+			if (pElev == pElevEnd)
+			{ // 末尾に達した場合
+
+				// while文を抜け出す
+				break;
+			}
+
+			// 次のオブジェクトを代入する
+			pElev = list.GetData(nIdx + 1);
+
+			// インデックスを加算する
+			nIdx++;
+		}
+	}
+
+	// 当たり判定状況を返す
+	return bCollision;
+}
+
+/*
+* @brief 六面体の当たり判定
+* @param pos [out] 対象の位置
+* @param posBlock [in] ブロックの位置
+* @param posOld [in] 対象の前回の位置
+* @param posOldBlock [in] ブロックの前回の位置
+* @param min [in] 最小値
+* @param minBlock [in] ブロックの最小値
+* @param max [in] 最大値
+* @param maxBlock [in] ブロックの最大値
+* @return bool ぶつかったかどうか
+*/
+bool collision::HexahedronCollision(D3DXVECTOR3* pos, const D3DXVECTOR3& posBlock, const D3DXVECTOR3& posOld, const D3DXVECTOR3& posOldBlock, const D3DXVECTOR3& min, const D3DXVECTOR3& minBlock, const D3DXVECTOR3& max, const D3DXVECTOR3& maxBlock)
+{
+	if (posBlock.y + maxBlock.y >= pos->y + min.y &&
+		posBlock.y + minBlock.y <= pos->y + max.y &&
+		posBlock.z + maxBlock.z >= pos->z + min.z &&
+		posBlock.z + minBlock.z <= pos->z + max.z)
+	{ // X軸の判定に入れる場合
+
+		if (posOldBlock.x + maxBlock.x <= posOld.x + min.x &&
+			posBlock.x + maxBlock.x >= pos->x + min.x)
+		{ // 右にぶつかった場合
+
+			// 位置を設定する
+			pos->x = posBlock.x + maxBlock.x - (min.x - COLLISION_ADD_DIFF_LENGTH);
+
+			// true を返す
+			return true;
+		}
+		else if (posOldBlock.x + minBlock.x >= posOld.x + max.x &&
+			posBlock.x + minBlock.x <= pos->x + max.x)
+		{ // 左にぶつかった場合
+
+			// 位置を設定する
+			pos->x = posBlock.x + minBlock.x - (max.x + COLLISION_ADD_DIFF_LENGTH);
+
+			// true を返す
+			return true;
+		}
+	}
+
+	if (posBlock.x + maxBlock.x >= pos->x + min.x &&
+		posBlock.x + minBlock.x <= pos->x + max.x &&
+		posBlock.y + maxBlock.y >= pos->y + min.y &&
+		posBlock.y + minBlock.y <= pos->y + max.y)
+	{ // Z軸の判定に入れる場合
+
+		if (posOldBlock.z + maxBlock.z <= posOld.z + min.z &&
+			posBlock.z + maxBlock.z >= pos->z + min.z)
+		{ // 奥にぶつかった場合
+
+			// 位置を設定する
+			pos->z = posBlock.z + maxBlock.z - (min.z - COLLISION_ADD_DIFF_LENGTH);
+
+			// true を返す
+			return true;
+		}
+		else if (posOldBlock.z + minBlock.z >= posOld.z + max.z &&
+			posBlock.z + minBlock.z <= pos->z + max.z)
+		{ // 手前にぶつかった場合
+
+			// 位置を設定する
+			pos->z = posBlock.z + minBlock.z - (max.z + COLLISION_ADD_DIFF_LENGTH);
+
+			// true を返す
+			return true;
+		}
+	}
+
+	if (posBlock.x + maxBlock.x >= pos->x + min.x &&
+		posBlock.x + minBlock.x <= pos->x + max.x &&
+		posBlock.z + maxBlock.z >= pos->z + min.z &&
+		posBlock.z + minBlock.z <= pos->z + max.z)
+	{ // Y軸の判定に入れる場合
+
+		if (posOldBlock.y + maxBlock.y <= posOld.y + min.y &&
+			posBlock.y + maxBlock.y >= pos->y + min.y)
+		{ // 上にぶつかった場合
+
+			// 位置を設定する
+			pos->y = posBlock.y + maxBlock.y - (min.y - COLLISION_ADD_DIFF_LENGTH);
+
+			// true を返す
+			return true;
+		}
+		else if (posOldBlock.y + minBlock.y >= posOld.y + max.y &&
+			posBlock.y + minBlock.y <= pos->y + max.y)
+		{ // 下にぶつかった場合
+
+			// 位置を設定する
+			pos->y = posBlock.y + minBlock.y - (max.y + COLLISION_ADD_DIFF_LENGTH);
+
+			// true を返す
+			return true;
+		}
+	}
+
+	// false を返す
+	return false;
+}
+
+/*
+* @brief 六面体の当たり判定(どの面に乗ったかの判定付き)
+* @param pos [out] 対象の位置
+* @param posBlock [in] ブロックの位置
+* @param posOld [in] 対象の前回の位置
+* @param posOldBlock [in] ブロックの前回の位置
+* @param min [in] 最小値
+* @param minBlock [in] ブロックの最小値
+* @param max [in] 最大値
+* @param maxBlock [in] ブロックの最大値
+* @return SCollision 六面体の当たり判定
+*/
+collision::SCollision collision::HexahedronClush(D3DXVECTOR3* pos, const D3DXVECTOR3& posBlock, const D3DXVECTOR3& posOld, const D3DXVECTOR3& posOldBlock, const D3DXVECTOR3& min, const D3DXVECTOR3& minBlock, const D3DXVECTOR3& max, const D3DXVECTOR3& maxBlock)
+{
+	// 当たり判定の変数を宣言
+	SCollision collision = { false,false,false,false,false,false };
+
+	if (posBlock.y + maxBlock.y >= pos->y + min.y &&
+		posBlock.y + minBlock.y <= pos->y + max.y &&
+		posBlock.z + maxBlock.z >= pos->z + min.z &&
+		posBlock.z + minBlock.z <= pos->z + max.z)
+	{ // X軸の判定に入れる場合
+
+		if (posOldBlock.x + maxBlock.x <= posOld.x + min.x &&
+			posBlock.x + maxBlock.x >= pos->x + min.x)
+		{ // 右にぶつかった場合
+
+			// 位置を設定する
+			pos->x = posBlock.x + maxBlock.x - (min.x - COLLISION_ADD_DIFF_LENGTH);
+
+			// 右の当たり判定を true にする
+			collision.bRight = true;
+		}
+		else if (posOldBlock.x + minBlock.x >= posOld.x + max.x &&
+			posBlock.x + minBlock.x <= pos->x + max.x)
+		{ // 左にぶつかった場合
+
+			// 位置を設定する
+			pos->x = posBlock.x + minBlock.x - (max.x + COLLISION_ADD_DIFF_LENGTH);
+
+			// 左の当たり判定を true にする
+			collision.bLeft = true;
+		}
+	}
+
+	if (posBlock.x + maxBlock.x >= pos->x + min.x &&
+		posBlock.x + minBlock.x <= pos->x + max.x &&
+		posBlock.y + maxBlock.y >= pos->y + min.y &&
+		posBlock.y + minBlock.y <= pos->y + max.y)
+	{ // Z軸の判定に入れる場合
+
+		if (posOldBlock.z + maxBlock.z <= posOld.z + min.z &&
+			posBlock.z + maxBlock.z >= pos->z + min.z)
+		{ // 奥にぶつかった場合
+
+			// 位置を設定する
+			pos->z = posBlock.z + maxBlock.z - (min.z - COLLISION_ADD_DIFF_LENGTH);
+
+			// 奥の当たり判定を true にする
+			collision.bFar = true;
+		}
+		else if (posOldBlock.z + minBlock.z >= posOld.z + max.z &&
+			posBlock.z + minBlock.z <= pos->z + max.z)
+		{ // 手前にぶつかった場合
+
+			// 位置を設定する
+			pos->z = posBlock.z + minBlock.z - (max.z + COLLISION_ADD_DIFF_LENGTH);
+
+			// 手前の当たり判定を true にする
+			collision.bNear = true;
+		}
+	}
+
+	if (posBlock.x + maxBlock.x >= pos->x + min.x &&
+		posBlock.x + minBlock.x <= pos->x + max.x &&
+		posBlock.z + maxBlock.z >= pos->z + min.z &&
+		posBlock.z + minBlock.z <= pos->z + max.z)
+	{ // Y軸の判定に入れる場合
+
+		if (posOldBlock.y + maxBlock.y <= posOld.y + min.y &&
+			posBlock.y + maxBlock.y >= pos->y + min.y)
+		{ // 上にぶつかった場合
+
+			// 位置を設定する
+			pos->y = posBlock.y + maxBlock.y - (min.y - COLLISION_ADD_DIFF_LENGTH);
+
+			// 上の当たり判定を true にする
+			collision.bTop = true;
+		}
+		else if (posOldBlock.y + minBlock.y >= posOld.y + max.y &&
+			posBlock.y + minBlock.y <= pos->y + max.y)
+		{ // 下にぶつかった場合
+
+			// 位置を設定する
+			pos->y = posBlock.y + minBlock.y - (max.y + COLLISION_ADD_DIFF_LENGTH);
+
+			// 下の当たり判定を true にする
+			collision.bBottom = true;
+		}
+	}
+
+	// 当たり判定の変数を返す
+	return collision;
+}
